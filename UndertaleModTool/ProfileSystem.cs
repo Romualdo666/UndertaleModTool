0376d42e260b2704e1cb1ef241d527aa3f2fb883
Lol124 <95558254+Romualdo666@users.noreply.github.com>
2024-10-04 23:44:08 -0300

    quickfix

--- UndertaleModTool/ProfileSystem.cs
+++ UndertaleModTool/ProfileSystem.cs
@@ -259,7 +259,10 @@ namespace UndertaleModTool
 
                                     var GetThisDir = File.ReadAllText(FileDir);
 
-                                    Input_text = File.ReadAllText(GetThisDir);
+                                    if (File.Exists(GetThisDir))
+                                        Input_text = File.ReadAllText(GetThisDir);
+                                    else
+                                        Input_text = SimpleTextInput("Loading Profile, please enter a Profile name.", "(Leaving this blank will name the profile with the data's MD5 hash.)", Input_text, true);
                                     //this.ShowMessage(Input_text);
                                 }
                                 else
